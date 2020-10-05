27d64bdd3055e96ee5e7eb8e2ae4277d9d74cdb2
Alexander Urbina <w.alexanderuc@gmail.com>
2020-10-04 19:18:23 -0600

    [UPDATE] Add IAM resources. Modify ECS Cluster to use FARGATE. Add web task definition resource.

--- resources.tf
+++ resources.tf
@@ -1,4 +1,42 @@
 # Create the AWS ECS Cluster
 resource "aws_ecs_cluster" "jitsi" {
   name = "jitsi-ecs-cluster"
+  capacity_providers = ["FARGATE"]
 }
+
+
+# Create IAM Role and policy
+resource "aws_iam_role" "ecsTaskExecutionRole" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = "${data.aws_iam_policy_document.assume_role_policy.json}"
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecsTaskExecutionRole_policy" {
+  role       = "${aws_iam_role.ecsTaskExecutionRole.name}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+
+# Create task definitions for Jitsi Services
+
+resource "aws_ecs_task_definition" "web" {
+  family = "web"
+  container_definitions = file("task_definitions/web.json")
+  requires_compatibilities = ["FARGATE"]
+  network_mode= "awsvpc"
+  memory = 512
+  cpu = 256 
+}
+
+
